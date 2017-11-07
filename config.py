29a392b717b6f95aa107a953bbfd0271c1d43b17
ennvita <ennvita@users.noreply.github.com>
2017-11-07 12:41:04 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-client_id = '377153776362061848' # bot client id
-token  = 'Mzc3MTUzNzc2MzYyMDYxODQ4.DOJDFg.hfYkAOBifDsOj5ewuNY1hJcUd7k' # bot token
+client_id = '' # bot client id
+token  = '' # bot token
 
 # weather_api = 'https://api.darksky.net/forecast/5cba58d586ac2f777d1457ad102afb16/' # darksky api
   # fix this, configure inside feathers.weather
