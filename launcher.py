dce98c5f7be02459345de4543371479f3f64e41a
ennvita <zoroasterbenashar@gmail.com>
2017-11-07 03:03:39 +0000

    initial commit on launcher.py

---
+++ launcher.py
@@ -0,0 +1,51 @@
+import asyncio
+import contextlib
+import click
+from bot import Birbbot
+
+try:
+	import uvloop
+except ImportError:
+	pass
+else:
+	asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+
+@contextlib.contextmanager
+def setup_logging():
+    try:
+        # __enter__
+        logging.getLogger('discord').setLevel(logging.INFO)
+        logging.getLogger('discord.http').setLevel(logging.WARNING)
+
+        log = logging.getLogger()
+        log.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename='Rooster.log', encoding='utf-8', mode='w')
+        dt_fmt = '%Y-%m-%d %H:%M:%S'
+        fmt = logging.Formatter('[{asctime}] [{levelname:<7}] {name}: {message}', dt_fmt, style='{')
+        handler.setFormatter(fmt)
+        log.addHandler(handler)
+
+        yield
+    finally:
+        # __exit__
+        handlers = log.handlers[:]
+        for hdlr in handlers:
+            hdlr.close()
+            log.removeHandler(hdlr)
+
+def run_bot():
+	loop = asyncio.get_event_loop()
+	bot = Birbbot()
+	bot.run()
+
+@click.group(invoke_without_command=True
+@click.pass_context
+def main(ctx):
+	if ctx.invoked_subcommand is None:
+		loop = asyncio.get_event_loop()
+		with setup_logging():
+			run.bot()
+
+if __name__ == '__main__':
+	main()
