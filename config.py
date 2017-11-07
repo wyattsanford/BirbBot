297b0348bbee3843f4cc2ebb7c33709343cee24b
ennvita <zoroasterbenashar@gmail.com>
2017-11-07 02:54:29 +0000

    updated config to include token comment out misc API

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 client_id = '377153776362061848' # bot client id
 TOKEN = 'Mzc3MTUzNzc2MzYyMDYxODQ4.DOJDFg.hfYkAOBifDsOj5ewuNY1hJcUd7k' # bot token
 
-weather_api = 'https://api.darksky.net/forecast/5cba58d586ac2f777d1457ad102afb16/' # darksky api
+# weather_api = 'https://api.darksky.net/forecast/5cba58d586ac2f777d1457ad102afb16/' # darksky api
   # fix this, configure inside feathers.weather
-google_key = '' # google maps api
+# google_key = '' # google maps api
 
 #configure here for feather (feathers.killwatch)
